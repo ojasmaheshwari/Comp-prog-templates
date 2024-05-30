3ce864cf5f7820621764678fe6f7d72cbe639ce9
ojasmaheshwari <104117027+ojasmaheshwari@users.noreply.github.com>
2024-05-30 17:21:16 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -5,7 +5,9 @@ using namespace std;
 #define ll long long
 #define umap unordered_map
 
-// template<typename T>
+template<typename T>
+T gcd(T a, T b) { T curr = 1; for (T i=1; i<=b; i++) {	if (a % i == 0 && b % i == 0) { curr = i;	} } return curr;}
+T lcm(T a, T b) { T start = max(a, b); T end = a*b; for (T i=start; i<end; i++) {	if ( i % a == 0 && i % b == 0 ) { return i;	} } return a*b;}
 int countOccr(string &s, string sub) { int occrs=0, pos=0; while ( (pos = s.find(sub, pos)) != string::npos ) { occrs++; pos += sub.length(); } return occrs;}
 
 void solve() {
