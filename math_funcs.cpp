1b81347ef3597aec7ecdab4bda99a6560034e845
ojasmaheshwari <104117027+ojasmaheshwari@users.noreply.github.com>
2024-05-30 17:20:49 +0530

    Update math_funcs.cpp

--- math_funcs.cpp
+++ math_funcs.cpp
@@ -1,21 +1,3 @@
-T gcd(T a, T b) {
-    T curr = 1;
-    for (T i=1; i<=b; i++) {
-	if (a % i == 0 && b % i == 0) {
-	    curr = i;
-	}
-    }
-    return curr;
-}
+T gcd(T a, T b) { T curr = 1; for (T i=1; i<=b; i++) {	if (a % i == 0 && b % i == 0) { curr = i;	} } return curr;}
 
-T lcm(T a, T b) {
-    T start = max(a, b);
-    T end = a*b;
-    for (T i=start; i<end; i++) {
-	if ( i % a == 0 && i % b == 0 ) {
-	    return i;
-	}
-    }
-    return a*b;
-}
-
+T lcm(T a, T b) { T start = max(a, b); T end = a*b; for (T i=start; i<end; i++) {	if ( i % a == 0 && i % b == 0 ) { return i;	} } return a*b;}
