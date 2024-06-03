924aca13482d44ca34e28e8adf01f616a4fe45a7
ojasmaheshwari <104117027+ojasmaheshwari@users.noreply.github.com>
2024-06-03 10:10:28 +0530

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 template<typename T>
 T gcd(T a, T b) { T curr = 1; for (T i=1; i<=b; i++) {	if (a % i == 0 && b % i == 0) { curr = i;	} } return curr;}
+template<typename T>
 T lcm(T a, T b) { T start = max(a, b); T end = a*b; for (T i=start; i<end; i++) {	if ( i % a == 0 && i % b == 0 ) { return i;	} } return a*b;}
 int countOccr(string &s, string sub) { int occrs=0, pos=0; while ( (pos = s.find(sub, pos)) != string::npos ) { occrs++; pos += sub.length(); } return occrs;}
 
